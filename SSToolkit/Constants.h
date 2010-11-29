de02c1c4eb8fd7de5c1b4fc505d9530180dce176
cricketgeek <jonesmc123@gmail.com>
2010-11-29 16:20:53 -0500

    added constants file for colors in SSToolkit

---
+++ SSToolkit/Constants.h
@@ -0,0 +1,17 @@
+//
+//  Constants.h
+//  SSToolkit
+//
+//  Created by Mark Jones on 11/29/10.
+//  Copyright 2010 Sam Soffes. All rights reserved.
+//
+
+#define RGB(r, g, b)  [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
+#define RGBA(r, g, b, a)  [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+
+#define BEIGE_COLOR		RGB(243,241,231)
+#define BROWN_COLOR		RGB(99,69,40)
+#define DARKBROWN_COLOR	RGB(43,25,15)
+#define GOLD_COLOR		RGB(233,216,138)
+#define SHADOW_COLOR	RGB(69,33,9) //shadow on wood
+#define COPPER_COLOR	RGBA(191,134,100,0.3) //table separator color
